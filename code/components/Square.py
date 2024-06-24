95e4f6fa387052fec6c11886bb076c45ced94473
Valeria <vesper.ludwig@outlook.com>
2024-06-23 21:57:27 -0400

    Moved the classes to the new file components. Implementation Code is under main.py

--- code/components/Square.py
+++ code/components/Square.py
@@ -2,13 +2,12 @@ class Square ():
     def __init__(self, file, rank, color):
         self.position = (file,rank)
         self.color = color
-
     def get_file(self):
         return self.position[0]
-    
     def get_rank(self):
         return self.position[1]
-    
     def __str__(self):
         square = f"{self.get_file()}{self.get_rank()} {self.color}"
         return square
+    
+    
---
+++ code/components/__init__.py
@@ -0,0 +1,3 @@
+from .Board import Board
+from .Piece import Piece
+from .Square import Square
