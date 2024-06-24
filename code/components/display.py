99f938eaa7c3ad08b95f927b75549e37e3413719
Valeria <vesper.ludwig@outlook.com>
2024-06-24 03:10:04 -0400

    .gitignore is now working

---
+++ code/components/display.py
@@ -0,0 +1,19 @@
+from components.Board import Board
+
+def display(object):
+    if type(object) is Board:
+        pass
+    elif type(object) is Square:
+        pass
+    elif type(object) is King:
+        pass  
+    elif type(object) is Queen:
+        pass
+    elif type(object) is Pawn:
+        pass
+    elif type(object) is Rook:
+        pass
+    elif type(object) is Bishop:
+        pass
+    elif type(object) is Knight:
+        pass
