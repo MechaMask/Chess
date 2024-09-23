032ca48c9bb72b568fa77b8adfea0905b9797572
Valeria <vesper.ludwig@outlook.com>
2024-09-23 17:45:36 -0400

    bitboard now has setitem() and getitem() with other ancillary methods, Board now implements bitboard

--- code/components/Bitboard.py
+++ code/components/Bitboard.py
@@ -19,8 +19,8 @@ def mask(mode, *args):
    if mode in mask_config:
         return mask_config[mode](*args)
 class bitboard:
-    def __init__(self): 
-        self.bits = 0
+    def __init__(self,bits=0): 
+        self.make(bits)
     
     def emptyboard(self):
         self.bits = mask('zs')
@@ -28,44 +28,40 @@ class bitboard:
     def fullboard(self):
         self.bits = mask('os',64)
 
-    def create(self, bits):
+    def make(self, bits):
         if type(bits) == int:
             #set bits to bits as is
-            assert(bits<=0xffffffffffffffff), "bitboard can contain at most 64 elements"
+            assert(bits<=0xffffffffffffffff), f"Invalid Input: {type(self).__name__} can contain at most 64 elements"
             self.bits = bits
         elif type(bits) == str:
             #the left most bit will be considered the most significant bit
-            assert(len(bits)<=64), "bitboard can contain at most 64 elements"
+            assert(len(bits)<=64), f"Invalid Input: {type(self).__name__} can contain at most 64 elements"
             self.bits = eval(f"0b{bits}")
         elif type(bits) == list:
             #bit[0] will be the least significant bit
-            assert(len(bits)<=64), "bitboard can contain at most 64 elements"
+            assert(len(bits)<=64), f"Invalid Input: {type(self).__name__} can contain at most 64 elements"
             value = 0
             for i in range(len(bits)):
-                assert bits[i] == 1 or bits[i] == 0, f"Invalid Input: a bit can only be {int} assigned as 1 or 0, got {bits[i]!r},"
+                assert bits[i] == 1 or bits[i] == 0, f"Invalid Input: A bit can only be {int} assigned as 1 or 0, got {bits[i]!r},"
                 value += 2**i if bits[i] == 1 else 0    
             self.bits = value
-
+    def fill(self,start=None,end=None):
+        start = 0 if start == None else start
+        end = len(bin(self.bits)[2:]) if end == None else end
+        return self.bits | 2**(end) - 2**(start)
     def invert_bit(self, k:int):
         self.bits ^= mask('nb',k) 
-        
     def set_bit(self, k:int):
         self.bits |= mask('nb',k)
-
     def clear_bit(self, k:int):
         self.bits &= mask('nz',k)
-
-    def get_bit(self, k:int) -> int:
-        return 1 & self.bits >> k
-    def set_to(self, bit:int, value:int):
-        if value == 1:
-            self.set_bit(bit)
-        elif value == 0:
-            self.clear_bit(bit)
-
+    def __setitem__(self,index,data):
+        assert(index < 64), f"IndexError: {type(self).__name__} assignment index out of range"
+        self.bits = (self.bits & ~(data<<index)) | data<<index
+    def __getitem__(self, index):
+        assert(index < 64), f"IndexError: {type(self).__name__} index out of range"
+        return 1 & (self.bits >> index)
     def __str__(self) -> str:
         return bin(0xffffffffffffffff-~self.bits)[3:]
     def __repr__(self) -> str:
         return f"{type(self).__name__}(Decimal:{self.bits},Hex:{hex(self.bits)},Binary:{self.__str__()})"
-
-a = bitboard()
--- code/components/Board.py
+++ code/components/Board.py
@@ -1,92 +1,98 @@
 from .Square import Square
+import bitboard
 import re
 class Board:
-    def __init__(self):
-        #a1__position = (0,0)
-        self.isflipped = False
-        self.board  = self.generate_empty()
-
-    def generate_start(self):
-        board = []
-        return board
-        
-    def generate_empty(self):
-        number_of_rows=8
-        number_of_columns=8
-        board = []
-      # A row is a rank. A board is stored from white's perspective
-      # as [row1,row2,row3,..]  such that row1 has squares A1 to H1 
-      #
-      # A column is a file. Each file Changes within each row.
-      # The board should look something like this [[A1,B1,C1,..],[[A2,B2,C2,..],...]]
-        colors = ("dark","light")
-        for rank_position in range(number_of_rows):
-            rank_squares  = [] #row of squares in a chess board 
-            color_switch = (0 + rank_position)%2
-            for file_position in range(number_of_columns):
-                file = chr(file_position + ord('A')) #get the letter (A-H) from file position (0-7)
-                rank = rank_position + 1 
-                color = colors[color_switch] 
-                color_switch = (color_switch + 1)%2 #aleternate colors per file
-                newsquare = Square(file,rank,color)
-                rank_squares.append(newsquare)
-            board.append(rank_squares)
-        return board 
     
-    def place_piece(self,piece, square):
-        #TODO should be able to handle chess notation or passing a square
-        if not square.occupied:
-            square.occupied = True
-            square.piece = piece
+    def __init__(self):
+        self.isflipped = False
+        self.full_board = bitboard.bitboard().fullboard()
+        self.board = {"light" : self.light ,"dark":self.dark,}
+        self.light,self.dark  = self.generate_start()
+        self.occupied_squares = self.or_boards(*{**self.light,**self.dark}.values())
+        
+    def or_boards(self,*bitboards):
+            res = bitboards[0]
+            for board in bitboards[1:]:
+                res.bits |=  board.bits
+            return res
+    def xor_boards(self,*bitboards):
+            res = bitboards[0]
+            for board in bitboards[1:]:
+                res.bits ^=  board.bits
+            return res
+    def generate_start(self):
+        light = {
+            "pawns" : bitboard.bitboard(0x000000000000ff00),
+            "rooks" : bitboard.bitboard(0x000000000000018),
+            "knights" : bitboard.bitboard(0x000000000000042),
+            "bishops":bitboard.bitboard(0x000000000000024),
+            "queens": bitboard.bitboard(0x0000000000000008),
+            "king": bitboard.bitboard(0x0000000000000010)}
+        dark = {
+            "pawns" : bitboard.bitboard(0x00ff000000000000),
+            "rooks" : bitboard.bitboard(0x1800000000000000),
+            "knights" : bitboard.bitboard(0x4200000000000000),
+            "bishops":bitboard.bitboard(0x2400000000000000),
+            "queens": bitboard.bitboard(0x0800000000000000),
+            "king": bitboard.bitboard(0x1000000000000000)}
+        
+        return light, dark
 
+    def generate_empty(self):
+        light ={
+            "pawns" : bitboard.bitboard(),
+            "rooks" : bitboard.bitboard(),
+            "knights" : bitboard.bitboard(),
+            "bishops":bitboard.bitboard(),
+            "queens": bitboard.bitboard(),
+            "king": bitboard.bitboard()} 
+        dark = {
+            "pawns" : bitboard.bitboard(),
+            "rooks" : bitboard.bitboard(),
+            "knights" : bitboard.bitboard(),
+            "bishops":bitboard.bitboard(),
+            "queens": bitboard.bitboard(),
+            "king": bitboard.bitboard()}
+        return light, dark
+    
     def legal_moves(self):
         pass
-
-    def get_square(self,square):
-        #TODO implement this logic in Square class then use it to get the indecices using get_coords
-        matched = re.search(r"[A-Ha-h]{1}[1-8]{1}",square)
-        if matched and (len(square) == 2):    
-            file = square[0].upper()  
-            rank = int(square[1]) 
-            first_ind = rank - 1  
-            second_ind = ord(file) - ord('A') 
-            return self.board[first_ind][second_ind]
     def flip_board(self):
         if self.isflipped:
             self.isflipped = False
         else:
             self.isflipped = True 
-        return self.isflipped
-    def display(self):
-        grid = ""
-        if (not self.isflipped):
-            for row in reversed(self.board):
-                row_squares = ""
-                for square in row:   
-                    row_squares += f"{square.display()}" 
-                grid += f"{row_squares}\n" 
-            print(grid)
-        else:
-            for row in self.board:
-                row_squares = ""
-                for square in reversed(row):
-                    row_squares += f"{square.display()} "
-                grid += f"{row_squares}\n"
-            print(grid)                
+        return self.isflipped   
+    # def display(self):
+    #     grid = ""
+    #     if (not self.isflipped):
+    #         for row in reversed(self.board):
+    #             row_squares = ""
+    #             for square in row:   
+    #                 row_squares += f"{square.display()}" 
+    #             grid += f"{row_squares}\n" 
+    #         print(grid)
+    #     else:
+    #         for row in self.board:
+    #             row_squares = ""
+    #             for square in reversed(row):
+    #                 row_squares += f"{square.display()} "
+    #             grid += f"{row_squares}\n"
+    #         print(grid)                
 
-    def __str__(self):
-        grid = ""
-        if (not self.isflipped):
-            for row in reversed(self.board):
-                row_squares = ""
-                for square in row:   
-                    row_squares += f"{str(square)} " 
-                grid += f"{row_squares}\n" 
-            return grid
-        else:
-            for row in self.board:
-                row_squares = ""
-                for square in reversed(row):
-                    row_squares += f"{str(square)} "
-                grid += f"{row_squares}\n"
-            return grid                         
+    # def __str__(self):
+    #     grid = ""
+    #     if (not self.isflipped):
+    #         for row in reversed(self.board):
+    #             row_squares = ""
+    #             for square in row:   
+    #                 row_squares += f"{str(square)} " 
+    #             grid += f"{row_squares}\n" 
+    #         return grid
+    #     else:
+    #         for row in self.board:
+    #             row_squares = ""
+    #             for square in reversed(row):
+    #                 row_squares += f"{str(square)} "
+    #             grid += f"{row_squares}\n"
+    #         return grid                         
